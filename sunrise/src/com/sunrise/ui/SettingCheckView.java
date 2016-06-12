099654afaca2438670d3c38102cf75bd9429df0d
miaozi002@163.com <miaozi002@163.com>
2016-06-12 20:55:59 +0800

    add

---
+++ sunrise/src/com/sunrise/ui/SettingCheckView.java
@@ -0,0 +1,27 @@
+package com.sunrise.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;;
+
+public class SettingCheckView extends LinearLayout {
+
+    public SettingCheckView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+
+
+    public SettingCheckView(Context context) {
+        super(context);
+        initView();
+    }
+
+    private void initView() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
